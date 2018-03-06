b237c331348bb09e24ab5e9f009bef86ecb20b9b
5398603098@qq.com <qwe123123>
2018-03-06 10:49:42 +0800

    route

---
+++ common/src/main/java/com/holyrobot/common/TripEntity.java
@@ -0,0 +1,112 @@
+package com.holyrobot.common;
+
+public class TripEntity {
+    private Integer id;
+
+    private String rouid;
+
+    private String departure;
+
+    private String destination;
+
+    private String price;
+
+    private String time;
+
+    private String spot;
+
+    private String hotel;
+
+    private String datasource;
+
+    private String url;
+    
+    
+
+    @Override
+	public String toString() {
+		return "TripEntity [id=" + id  + ", hotel=" + hotel +  ", rouid=" + rouid + ", departure=" + departure + ", destination=" + destination
+				+ ", price=" + price + ", time=" + time + ", spot=" + spot + ", datasource="
+				+ datasource + ", url=" + url + "]";
+	}
+
+	public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRouid() {
+        return rouid;
+    }
+
+    public void setRouid(String rouid) {
+        this.rouid = rouid == null ? null : rouid.trim();
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure == null ? null : departure.trim();
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination == null ? null : destination.trim();
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price == null ? null : price.trim();
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time == null ? null : time.trim();
+    }
+
+    public String getSpot() {
+        return spot;
+    }
+
+    public void setSpot(String spot) {
+        this.spot = spot == null ? null : spot.trim();
+    }
+
+    public String getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(String hotel) {
+        this.hotel = hotel == null ? null : hotel.trim();
+    }
+
+    public String getDatasource() {
+        return datasource;
+    }
+
+    public void setDatasource(String datasource) {
+        this.datasource = datasource == null ? null : datasource.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+}
