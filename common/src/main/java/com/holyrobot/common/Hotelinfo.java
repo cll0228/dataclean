da446125059d584c91ea3b93a049da07c49785e9
cuiliulong <3402069362@qq.com>
2018-03-08 09:18:18 +0800

    序列化接口类

--- common/src/main/java/com/holyrobot/common/Hotelinfo.java
+++ common/src/main/java/com/holyrobot/common/Hotelinfo.java
@@ -3,7 +3,9 @@ package com.holyrobot.common;
 import java.io.Serializable;
 import java.util.Date;
 
-public class Hotelinfo implements Serializable{
+public class Hotelinfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private String id;
 
