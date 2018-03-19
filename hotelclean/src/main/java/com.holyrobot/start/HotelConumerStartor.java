da4d13e2b25ffa10fbe31a5c19b27399e7e3bb63
cuiliulong <3402069362@qq.com>
2018-03-19 09:43:06 +0800

    test git commit jenkins pull

--- hotelclean/src/main/java/com.holyrobot/start/HotelConumerStartor.java
+++ hotelclean/src/main/java/com.holyrobot/start/HotelConumerStartor.java
@@ -24,6 +24,8 @@ import java.util.concurrent.TimeUnit;
 public class HotelConumerStartor {
     private static final Logger logger = LoggerFactory.getLogger(HotelConumerStartor.class);
 
+
+
     final static ThreadPoolExecutor executor = new ThreadPoolExecutor(100, 150, 5, TimeUnit.SECONDS, new LinkedBlockingDeque<>());
 
 
